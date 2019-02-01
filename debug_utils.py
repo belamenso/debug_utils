79d2d3932b2cc6ec33fdc771df9da2a8cc74e2e8
Bartosz Białas <belamenso@gmail.com>
2019-02-01 17:01:51 +0100

    Added suppport for pretty printing lists

--- debug_utils.py
+++ debug_utils.py
@@ -33,6 +33,67 @@ def benchmark(f, rounds=100):
         if (i + 1) % 10 == 0:
             print('{} {:3.2f} {:3.2f}'. format(i + 1, mean, statistics.stdev(times, mean)))
 
+def print_list(xs, indices, str_labels):
+
+    labels = str_labels.split(' ')
+    assert len(indices) == len(labels)
+    assert len(set(labels)) == len(labels)
+
+    def colorful(s, label=None):
+        colors = list(range(31, 38)) + list(range(90, 97))
+        if label is None:
+            return (str(s),) * 2
+        else:
+            color = colors[labels.index(label)]
+            return str(s), '\033[{}m{}\033[0m'.format(color, str(s))
+
+    index_locations = {}
+
+    middle_line = ''
+    middle_line_color = ''
+    for i in range(-1, len(xs) + 1):
+        found = None
+        for jj, j in enumerate(indices):
+            if j == i:
+                index_locations[i] = 0 if i == -1 else len(middle_line) + 1
+                if not found: found = labels[jj]
+        str_output = '(' if i == -1 else ' )' if i == len(xs) else (' ' + str(xs[i]))
+        _, __ = colorful(str_output, found)
+        middle_line += _
+        middle_line_color += __
+
+    top_line_chars = [' '] * len(middle_line)
+    for k, v in index_locations.items():
+        top_line_chars[v] = colorful('↓', labels[indices.index(k)])[1]
+    top_line = ''.join(top_line_chars)
+
+    labels_and_indices = []
+    for k, v in index_locations.items():
+        for ii, i in enumerate(indices):
+            if i == k:
+                labels_and_indices.append((labels[ii], k))
+    labels_and_indices.sort(key=lambda p: p[1])
+    processed_pairs = []
+    for i in range(len(labels_and_indices)):
+        if not processed_pairs or labels_and_indices[i][1] != processed_pairs[-1][1]:
+            processed_pairs.append([labels_and_indices[i][0], labels_and_indices[i][1], labels_and_indices[i][0]])
+        else:
+            processed_pairs[-1][0] += '=' + labels_and_indices[i][0]
+    bottom_line = ''
+    for names, val, first_name in processed_pairs:
+        if bottom_line != '':
+            bottom_line += ' '
+        first = True
+        for name in names.split('='):
+            if not first: bottom_line += '='
+            bottom_line += colorful(name, first_name)[1]
+            first = False
+        bottom_line += '=' + str(val)
+
+    print(top_line)
+    print(middle_line_color)
+    print(bottom_line)
+
 
 if __name__ == '__main__':
-    pass
+    print_list([2131,2,3,5,4300325,1,2,3,5,2,2], [0,2,2,2,5], 'a b c beg end')
